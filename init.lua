d520e3962a55b64ed451c1091c1db6d3fe6de86c
vivek tiwari <vitiwari@bmc.com>
2017-02-08 12:43:16 +0530

    Initial redesign/development of plugin

--- init.lua
+++ init.lua
@@ -14,93 +14,77 @@
 
 local framework = require('framework')
 local json = require('json')
-local dns = require('dns')
+local table = require('table')
 local Plugin = framework.Plugin 
 local WebRequestDataSource = framework.WebRequestDataSource
 local notEmpty = framework.string.notEmpty
 local auth = framework.util.auth
-local hasAny = framework.table.hasAny
 local clone = framework.table.clone
-local table = require('table')
-local find = framework.table.find
 local percentage = framework.util.percentage
 local isHttpSuccess = framework.util.isHttpSuccess
+local PollerCollection = framework.PollerCollection
+local DataSourcePoller = framework.DataSourcePoller
+local ipack = framework.util.ipack
 
 local params = framework.params
-params.pollInterval = notEmpty(tonumber(params.pollInterval), 1000)
-params.host = notEmpty(params.host, 'localhost')
+params.pollInterval = notEmpty(tonumber(params.pollInterval), 5000)
+params.host = notEmpty(params.host, '127.0.0.1')
 params.port = notEmpty(params.port, '8091')
-params.advanced_metrics = params.advanced_metrics or false
 
--- 1. get /pools/nodes get the node based on the host parameter.
--- 2. get /pools/default/buckets for a list of available buckets for advanced metrics. This can be done with a CachedWebRequestDataSource
--- 3. get /pools/default/buckets/%s/nodes/%s/stats?zoom=minute for each bucket for advanced metrics. This can be a list of child request returned by the chain function.
+-- 1. get /pools/nodes get the node & cluster based on the host parameter .
+-- 2. get /pools/default/buckets for a list of available buckets for advanced metrics. 
+-- 3. get /pools/default/buckets/%s/stats for each bucket for bucket level advanced metrics. .
 
 local buckets = {}
+local bucketCount = 0
 local pending_requests = {}
 local target
-local plugin
-
-local function getNode(nodes, hostname)
-  return find(function (v) return v.hostname == hostname end, nodes) 
-end
-
-local function standardMetrics(cluster)
-  local result = {}
-  result['COUCHBASE_RAM_QUOTA_TOTAL'] = cluster.storageTotals.ram.quotaTotalPerNode or 0
-  result['COUCHBASE_RAM_QUOTA_USED'] = cluster.storageTotals.ram.quotaUsedPerNode or 0
-
-  local node = getNode(cluster.nodes, target)
-  if node then
-    result['COUCHBASE_CPU_USAGE_RATE'] = percentage(node.systemStats.cpu_utilization_rate or 0)
-    result['COUCHBASE_RAM_SYSTEM_TOTAL'] = node.systemStats.mem_total or 0
-    result['COUCHBASE_RAM_SYSTEM_FREE'] = node.systemStats.mem_free or 0
-    result['COUCHBASE_SWAP_TOTAL'] = node.systemStats.swap_total or 0
-    result['COUCHBASE_SWAP_USED'] = node.systemStats.swap_used or 0
-    result['COUCHBASE_OPERATIONS'] = node.interestingStats.ops or 0
-    result['COUCHBASE_DOCUMENTS_COUNT'] = node.interestingStats.curr_items or 0
-    result['COUCHBASE_DOCUMENTS_SIZE'] = node.interestingStats.couch_docs_data_size or 0
-    result['COUCHBASE_DOCUMENTS_SIZE_ON_DISK'] = node.interestingStats.couch_docs_actual_disk_size or 0
-    result['COUCHBASE_VIEWS_SIZE'] = node.interestingStats.couch_views_data_size or 0
-    result['COUCHBASE_VIEWS_SIZE_ON_DISK'] = node.interestingStats.couch_views_actual_disk_size or 0
-  end
-  
-  return result
-end
-
 local options = {}
 options.host = params.host
 options.port = params.port
 options.auth = auth(params.username, params.password)
 options.path = '/pools/nodes'
+options.source = notEmpty(params.source,params.host)
 options.wait_for_end = true
-local ds = WebRequestDataSource:new(options)
-ds:chain(function (context, callback, data)
-  local cluster = json.parse(data)
-  
-  local metrics = standardMetrics(cluster)
-  plugin:report(metrics)
+options.meta = "clusterreq"
 
-  if params.advanced_metrics then
-    local data_sources = {}
-    for i, bucket in ipairs(buckets) do
-      local node = target 
-      local opts = clone(options)
-      opts.meta = bucket
-      opts.path = ('/pools/default/buckets/%s/nodes/%s/stats?zoom=minute'):format(bucket, node)
-      local child_ds = WebRequestDataSource:new(opts)
-      child_ds:propagate('error', context)
-      table.insert(data_sources, child_ds)
-      pending_requests[bucket] = true
-    end
-    return data_sources
+
+
+local function clusterNodeStatsExtractor (data, item)
+	local result = {}
+	local function metric(...)
+      	ipack(result, ...)
+	end
+	
+	local src = options.source
+	metric('COUCHBASE_CLSTR_RAM_QUOTA_TOTAL', data.storageTotals.ram.quotaTotal,nil,src)
+	metric('COUCHBASE_CLSTR_RAM_QUOTA_USED', data.storageTotals.ram.quotaUsed,nil,src)
+    metric('COUCHBASE_CLSTR_HDD_QUOTA_TOTAL', data.storageTotals.hdd.quotaTotal,nil,src)
+    metric('COUCHBASE_CLSTR_HDD_USED', data.storageTotals.hdd.used,nil,src)
+  local count=0
+  for i, node in ipairs(data.nodes) do
+    count=count+1
+    local ip, port = node.hostname:match'(.-):(.*)'
+    local src1=src.."_"..ip;   
+    metric('COUCHBASE_NODE_CPU_USAGE_RATE', node.systemStats.cpu_utilization_rate,nil,src1)
+    metric('COUCHBASE_NODE_RAM_SYSTEM_TOTAL', node.systemStats.mem_total,nil,src1)
+    metric('COUCHBASE_NODE_RAM_SYSTEM_FREE', node.systemStats.mem_free,nil,src1)
+    metric('COUCHBASE_NODE_SWAP_TOTAL', node.systemStats.swap_used,nil,src1)
+    metric('COUCHBASE_NODE_SWAP_USED', node.systemStats.swap_total,nil,src1)
+    metric('COUCHBASE_NODE_OPERATIONS', node.interestingStats.ops,nil,src1)
+    metric('COUCHBASE_NODE_DOCUMENTS_COUNT', node.interestingStats.curr_items,nil,src1)
+    metric('COUCHBASE_NODE_DOCUMENTS_SIZE', node.interestingStats.couch_docs_data_size,nil,src1)
+    metric('COUCHBASE_NODE_DOCUMENTS_SIZE_ON_DISK', node.interestingStats.couch_docs_actual_disk_size,nil,src1)
+    metric('COUCHBASE_NODE_VIEWS_SIZE', node.interestingStats.couch_views_data_size,nil,src1)
+    metric('COUCHBASE_NODE_VIEWS_SIZE_ON_DISK', node.interestingStats.couch_views_actual_disk_size,nil,src1)
   end
-end)
+   metric('COUCHBASE_CLSTR_NODE_COUNT', count,nil,src)
+   metric('COUCHBASE_CLSTR_BUCKET_COUNT',bucketCount,nil,src)
+	return result
+end
 
--- For the standard metrics we look for the node that has the same IP and port as the specified parameters.
--- For the advanced metrics we get a list of buckets and then agregate bucket metrics
 local stats_total_tmpl = {
-  couch_views_fragmentation = 0,
+  couch_docs_fragmentation = 0,
   couch_views_fragmentation = 0,
   couch_views_ops = 0,
   avg_disk_commit_time = 0,
@@ -111,67 +95,82 @@ local stats_total_tmpl = {
   evictions = 0,
   misses = 0,
   xdc_ops = 0,
-  replication_changes_left = 0,
-  major_faults = 0,
-  minor_faults = 0,
-  page_faults = 0
+  replication_changes_left = 0
 }
 
-local stats_total = clone(stats_total_tmpl)
-
-plugin = Plugin:new(params, ds)
-function plugin:onParseValues(data, extra)
-  if not extra.info then
-    return
+local function bucketStatsExtractor (data1, item , bucketName)
+  local data = clone(stats_total_tmpl)
+  for k, v in pairs(data) do
+    local samples = data1.op.samples[k] or {} 
+    data[k] = v + tonumber(samples[#samples] or 0)
   end
-
-  local parsed = json.parse(data)
-  for k, v in pairs(stats_total) do
-    local samples = parsed.op.samples[k] or {} 
-    stats_total[k] = v + tonumber(samples[#samples] or 0)
-  end
-
-  pending_requests[extra.info] = nil
-  if not hasAny(pending_requests) then
-    local result = {}
-    result['COUCHBASE_DOCUMENTS_FRAGMENTATION'] = percentage(stats_total.couch_views_fragmentation)
-    result['COUCHBASE_VIEWS_FRAGMENTATION'] = percentage(stats_total.couch_views_fragmentation)
-    result['COUCHBASE_VIEWS_OPERATIONS'] = stats_total.couch_views_ops
-    result['COUCHBASE_DISK_COMMIT_TIME'] = stats_total.avg_disk_commit_time
-    result['COUCHBASE_DISK_UPDATE_TIME'] = stats_total.avg_disk_update_time
-    result['COUCHBASE_CAS_HITS'] = stats_total.cas_hits
-    result['COUCHBASE_CAS_MISSES'] = stats_total.cas_misses
-    result['COUCHBASE_DISK_FETCHES'] = stats_total.ep_bg_fetched
-    result['COUCHBASE_EVICTIONS'] = stats_total.evictions
-    result['COUCHBASE_MISSES'] = stats_total.misses
-    result['COUCHBASE_XDCR_OPERATIONS'] = stats_total.xdc_ops
-    result['COUCHBASE_REPLICATION_CHANGES_LEFT'] = stats_total.replication_changes_left
-    result['COUCHBASE_MAJOR_FAULTS'] = stats_total.major_faults
-    result['COUCHBASE_MINOR_FAULTS'] = stats_total.minor_faults
-    result['COUCHBASE_PAGE_FAULTS'] = stats_total.page_faults
   
-    stats_total = clone(stats_total_tmpl)
-    return result
-  end
+	local result = {}
+	local metric = function (...) ipack(result, ...) end
+	local src = options.source .. ".bucket."..bucketName
+	metric('COUCHBASE_BUCKET_DOC_FRAGMENTATION', percentage(data.couch_docs_fragmentation),nil,src)
+	metric('COUCHBASE_BUCKET_VIEWS_FRAGMENTATION', percentage(data.couch_views_fragmentation),nil,src)
+  metric('COUCHBASE_BUCKET_VIEWS_OPERATIONS', data.couch_views_ops,nil,src)
+  metric('COUCHBASE_BUCKET_DISK_COMMIT_TIME', data.avg_disk_commit_time,nil,src)
+  metric('COUCHBASE_BUCKET_DISK_UPDATE_TIME', data.avg_disk_update_time,nil,src)
+  metric('COUCHBASE_BUCKET_CAS_HITS', data.cas_hits,nil,src)
+  metric('COUCHBASE_BUCKET_CAS_MISSES', data.cas_misses,nil,src)
+  metric('COUCHBASE_BUCKET_DISK_FETCHES', data.ep_bg_fetched,nil,src)
+  metric('COUCHBASE_BUCKET_EVICTIONS', data.evictions,nil,src)
+  metric('COUCHBASE_BUCKET_MISSES', data.misses,nil,src)
+  metric('COUCHBASE_BUCKET_XDCR_OPERATIONS', data.xdc_ops,nil,src)
+  metric('COUCHBASE_BUCKET_REPL_CHANGES_LEFT', data.replication_changes_left,nil,src)
+	return result
 end
 
-local function resolveHost(host)
-  dns.resolve(host, function (failure, addresses)
-    if failure then
-      plugin:emitEvent('critical', ('%s Unresolved'):format(host), host, host, ('Could not resolve the %s'):format(host))
-      return
-    end
 
-    target = addresses[1] .. ':' .. params.port 
-    plugin:run()
-  end)
-end
+
+	local function createOptions(item)
+
+		local options = {}
+		options.host = item.host
+		options.port = item.port
+		options.wait_for_end = true
+		options.auth=auth(item.username, item.password)
+		options.source = item.source
+		return options
+	end
+	local function createClusterStats(item)
+		local options = createOptions(item)
+		options.path = "/pools/nodes"
+		options.meta = "clusterreq"
+		return WebRequestDataSource:new(options)
+	end	
+	
+	local function createBucketStats(item,bucket)
+		local options = createOptions(item)
+		options.path = '/pools/default/buckets/'..bucket..'/stats'
+		options.meta = bucket
+		return WebRequestDataSource:new(options)
+	end	
+	
+	local function createPollers()
+		local pollers = PollerCollection:new()
+
+		local cs = createClusterStats(params)
+		local clusterStatsPoller = DataSourcePoller:new(params.pollInterval, cs)
+		pollers:add(clusterStatsPoller)
+     	print(" buckets have length"..#buckets)
+		for i, bucket in ipairs(buckets) do
+      local bs = createBucketStats(params,bucket)
+			local bucketStatsPoller = DataSourcePoller:new(params.pollInterval, bs)
+			pollers:add(bucketStatsPoller)
+      bucketCount=bucketCount+1
+		end 
+	  return pollers
+	end
+ 
 
 local function run()
   local opts = clone(options)
   opts.path = '/pools/default/buckets'
   local buckets_ds = WebRequestDataSource:new(opts)
-  buckets_ds:propagate('error', plugin)
+ -- buckets_ds:propagate('error', plugin)
   buckets_ds:fetch(nil, function (data, extra) 
     if not isHttpSuccess(extra.status_code) then
       plugin:emitEvent('error', 'Http Error', params.host, params.host, ('Http status code %s'):format(extra.status_code))
@@ -181,7 +180,23 @@ local function run()
     for _, bucket in ipairs(parsed) do
       table.insert(buckets, bucket.name)
     end
-    resolveHost(params.host)
+    local pollers = createPollers();
+	local plugin = Plugin:new(params, pollers)
+	
+	function plugin:onParseValues(data, extra)
+				if not extra.info then
+				  return
+				end
+				local parsed = json.parse(data)
+				local result= {}
+				if extra.info == "clusterreq" then
+				  result= clusterNodeStatsExtractor(parsed , options)
+				else
+				  result= bucketStatsExtractor( parsed , options,extra.info)
+				end
+		return result
+	end		
+	plugin:run()
   end)
 end
 
